3b1975bc890b1f600cfa5108029f7af1101ad3ad
Saumya Rawal <114496132+technoprogramming92@users.noreply.github.com>
2025-01-24 18:27:07 +0530

    disabled view code

---
+++ js/disable-view-source.js
@@ -0,0 +1,10 @@
+document.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && e.key === "u") {
+    e.preventDefault();
+  }
+});
+
+// Disable right-click context menu
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
